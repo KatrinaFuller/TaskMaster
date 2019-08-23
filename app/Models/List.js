6369ecefbf748215475ad6ad98f331548abe8c89
Katrina Fuller <trinafuller@att.net>
2019-08-23 12:01:02 -0600

    created model for list

--- app/Models/List.js
+++ app/Models/List.js
@@ -1,4 +1,11 @@
 export default class List {
     //TODO You will need to create a constructor 
     //and the methods needed to create the view template for this model
+    constructor(data) {
+        this.title = data.title
+        this.task = data.task
+        this.date = data.date
+        this.dueDate = data.dueDate
+        this.priorityLevel = data.priorityLevel
+    }
 }
